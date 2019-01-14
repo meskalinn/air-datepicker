39b642796335c934c24b9836418537e22cfaaf6d
meskalinn <46692754+meskalinn@users.noreply.github.com>
2019-01-15 00:49:42 +0300

    Update datepicker.tr.js

--- src/js/i18n/datepicker.tr.js
+++ src/js/i18n/datepicker.tr.js
@@ -1,1 +1,15 @@
-alert
+;
+(function ($) {
+    $.fn.datepicker.language['tr'] = {
+        days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+        daysShort: ['Paz', 'Pzt', 'Sal', 'Çar', 'Per', 'Cu', 'Cmt'],
+        daysMin: ['Pz', 'Pzt', 'Sa', 'Ça', 'Pe', 'Cu', 'Cmt'],
+        months: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+        monthsShort: ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Agu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+        today: 'Bugün',
+        clear: 'Temizle',
+        dateFormat: 'mm/dd/yyyy',
+        timeFormat: 'hh:ii aa',
+        firstDay: 0
+    };
+})(jQuery);
